91111cedd9f8e1000611ae4de571dd7e7ad06abb
Staffan Olsson <staffan@repos.se>
2017-11-16 11:48:11 +0100

    Extract export function so we can reuse for non-namespaced resources

--- export.sh
+++ export.sh
@@ -7,6 +7,21 @@ echo "Exporting to $EXPORT"
 
 namespaces=$(kubectl get namespaces -o=jsonpath='{.items[*].metadata.name}')
 
+getexport() {
+    N="$1"
+    R="$2"
+
+    type=$(echo $R | awk -F '/' '{ print $1 }')
+    name=$(echo $R | awk -F '/' '{ print $2 }')
+    mkdir -p "$EXPORT/$N/$type"
+    echo -n "# ($type) $name, lines of yaml: "
+    dest="$EXPORT/$N/$type/$name"
+    kubectl --namespace=$N get $R --export -o=yaml | tee "$dest.yml" | wc -l
+    grep -q 'kubernetes.io/created-by:' "$dest.yml" && echo "# ... is a generated resource" && \
+        mv "$dest.yml" "$dest.k8s-created.yml"
+    : # don't exit on missing created-by
+}
+
 for N in $namespaces; do
     mkdir -p "$EXPORT/$N"
     
@@ -16,14 +31,7 @@ for N in $namespaces; do
     )
     echo "### namespace $N: $(echo "$all" | wc -l) resources"
     for R in $all; do
-        type=$(echo $R | awk -F '/' '{ print $1 }')
-        name=$(echo $R | awk -F '/' '{ print $2 }')
-        mkdir -p "$EXPORT/$N/$type"
-        echo -n "# ($type) $name, lines of yaml: "
-        dest="$EXPORT/$N/$type/$name"
-        kubectl --namespace=$N get $R --export -o=yaml | tee "$dest.yml" | wc -l
-        grep -q 'kubernetes.io/created-by:' "$dest.yml" && echo "# ... is a generated resource" && \
-          mv "$dest.yml" "$dest.k8s-created.yml"
+        getexport "$N" "$R"
     done
 
 done
