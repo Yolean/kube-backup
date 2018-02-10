0bf808644afd4791ca0cb598fb762600bb4e1f3b
solsson <solsson@gmail.com>
2018-02-10 15:02:52 +0100

    Ah

--- export.sh
+++ export.sh
@@ -29,7 +29,7 @@ getexport() {
         mv "$dest.yml" "$dest.k8s-created.yml"
     [ -f "$dest.yml" ] && grep -q 'generateName:' "$dest.yml" && echo "# ... is a generated resource" && \
         mv "$dest.yml" "$dest.k8s-created.yml"
-    [ -f "$dest.yml" ] && grep -q 'ownerReference:' "$dest.yml" && echo "# ... is an owned resource" && \
+    [ -f "$dest.yml" ] && grep -q 'ownerReferences:' "$dest.yml" && echo "# ... is an owned resource" && \
         mv "$dest.yml" "$dest.k8s-created.yml"
     : # don't exit on missing created-by
 }
